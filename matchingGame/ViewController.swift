cd78f741e505360ebf569b76a3fc504bdbb37ab0
liuy363 <liuy363@mcmaster.ca>
2019-12-30 21:35:21 -0500

    Initial Commit

---
+++ matchingGame/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  matchingGame
+//
+//  Created by Cynthia on 30/12/2019.
+//  Copyright © 2019 Cynthia. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
